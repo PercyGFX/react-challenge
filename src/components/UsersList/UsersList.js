3e1958f45fb5b4830262b725b230f838e215a85a
PercyGFX <isurangabtk@gmail.com>
2025-01-07 17:11:29 +0530

    design user card view

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -4,7 +4,7 @@ import { useUsers } from "../../hooks/useUsers";
 const UsersList = () => {
   const { data: users, isLoading, isError } = useUsers();
 
-  console.log(users);
+  //console.log(users);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -12,7 +12,34 @@ const UsersList = () => {
 
   return (
     // User List goes here...
-    <div>hjg</div>
+    <div>
+      <div>
+        <h1 className="text-2xl font-semibold mb-4">Users List</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-3">
+          {users?.users?.map((user) => (
+            <div
+              key={user.id}
+              className="bg-gray-100 p-4 rounded-lg shadow-md flex gap-x-3 items-center"
+            >
+              <div>
+                <img
+                  className=" rounded-full h-22 w-22 object-cover"
+                  src={user.image}
+                  alt={user.firstName}
+                />{" "}
+              </div>
+              <div className=" gap-y-3">
+                <h2 className="text-xl font-semibold">
+                  {user.firstName} {user.lastName}
+                </h2>
+                <p className="text-gray-500">{user.email}</p>
+                <p className="text-gray-500">{user.phone}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
